6ba952db157d8658d0ebec7df41b8effd73d79a7
buczu1612 <pjbuczynski@gmail.com>
2019-11-17 18:01:17 +0100

    Add project files.

---
+++ WindowsFormSQL/MainWindow.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace WindowsFormSQL
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.valueLabel = new System.Windows.Forms.Label();
+            this.commentLabel = new System.Windows.Forms.Label();
+            this.valueTextBox = new System.Windows.Forms.TextBox();
+            this.commentTextBox = new System.Windows.Forms.TextBox();
+            this.insertButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.idListBox = new System.Windows.Forms.ListBox();
+            this.timeListBox = new System.Windows.Forms.ListBox();
+            this.valueListBox = new System.Windows.Forms.ListBox();
+            this.commentListBox = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.formOfDatePresentationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listBoxesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridViewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.measurementsDataSet = new WindowsFormSQL.MeasurementsDataSet();
+            this.measurementsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.measurementsTableAdapter = new WindowsFormSQL.MeasurementsDataSetTableAdapters.MeasurementsTableAdapter();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.timeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.valueDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.commentDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.measurementsDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.measurementsBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // valueLabel
+            // 
+            this.valueLabel.AutoSize = true;
+            this.valueLabel.Location = new System.Drawing.Point(35, 35);
+            this.valueLabel.Name = "valueLabel";
+            this.valueLabel.Size = new System.Drawing.Size(34, 13);
+            this.valueLabel.TabIndex = 0;
+            this.valueLabel.Text = "Value";
+            // 
+            // commentLabel
+            // 
+            this.commentLabel.AutoSize = true;
+            this.commentLabel.Location = new System.Drawing.Point(35, 64);
+            this.commentLabel.Name = "commentLabel";
+            this.commentLabel.Size = new System.Drawing.Size(51, 13);
+            this.commentLabel.TabIndex = 1;
+            this.commentLabel.Text = "Comment";
+            // 
+            // valueTextBox
+            // 
+            this.valueTextBox.Location = new System.Drawing.Point(92, 32);
+            this.valueTextBox.Name = "valueTextBox";
+            this.valueTextBox.Size = new System.Drawing.Size(100, 20);
+            this.valueTextBox.TabIndex = 2;
+            // 
+            // commentTextBox
+            // 
+            this.commentTextBox.Location = new System.Drawing.Point(92, 61);
+            this.commentTextBox.Name = "commentTextBox";
+            this.commentTextBox.Size = new System.Drawing.Size(100, 20);
+            this.commentTextBox.TabIndex = 3;
+            // 
+            // insertButton
+            // 
+            this.insertButton.BackColor = System.Drawing.Color.Gold;
+            this.insertButton.Location = new System.Drawing.Point(264, 35);
+            this.insertButton.Name = "insertButton";
+            this.insertButton.Size = new System.Drawing.Size(109, 42);
+            this.insertButton.TabIndex = 4;
+            this.insertButton.Text = "Insert";
+            this.insertButton.UseVisualStyleBackColor = false;
+            this.insertButton.Click += new System.EventHandler(this.insertButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.BackColor = System.Drawing.Color.Chartreuse;
+            this.deleteButton.Location = new System.Drawing.Point(381, 35);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(109, 42);
+            this.deleteButton.TabIndex = 5;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = false;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // updateButton
+            // 
+            this.updateButton.BackColor = System.Drawing.Color.RoyalBlue;
+            this.updateButton.Location = new System.Drawing.Point(496, 35);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(109, 42);
+            this.updateButton.TabIndex = 6;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = false;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // idListBox
+            // 
+            this.idListBox.FormattingEnabled = true;
+            this.idListBox.Location = new System.Drawing.Point(38, 128);
+            this.idListBox.Name = "idListBox";
+            this.idListBox.Size = new System.Drawing.Size(51, 381);
+            this.idListBox.TabIndex = 7;
+            // 
+            // timeListBox
+            // 
+            this.timeListBox.FormattingEnabled = true;
+            this.timeListBox.Location = new System.Drawing.Point(95, 128);
+            this.timeListBox.Name = "timeListBox";
+            this.timeListBox.Size = new System.Drawing.Size(150, 381);
+            this.timeListBox.TabIndex = 8;
+            // 
+            // valueListBox
+            // 
+            this.valueListBox.FormattingEnabled = true;
+            this.valueListBox.Location = new System.Drawing.Point(251, 128);
+            this.valueListBox.Name = "valueListBox";
+            this.valueListBox.Size = new System.Drawing.Size(122, 381);
+            this.valueListBox.TabIndex = 9;
+            // 
+            // commentListBox
+            // 
+            this.commentListBox.FormattingEnabled = true;
+            this.commentListBox.Location = new System.Drawing.Point(379, 128);
+            this.commentListBox.Name = "commentListBox";
+            this.commentListBox.Size = new System.Drawing.Size(226, 381);
+            this.commentListBox.TabIndex = 10;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.formOfDatePresentationToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1130, 24);
+            this.menuStrip1.TabIndex = 12;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // formOfDatePresentationToolStripMenuItem
+            // 
+            this.formOfDatePresentationToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.listBoxesToolStripMenuItem,
+            this.dataGridViewToolStripMenuItem});
+            this.formOfDatePresentationToolStripMenuItem.Name = "formOfDatePresentationToolStripMenuItem";
+            this.formOfDatePresentationToolStripMenuItem.Size = new System.Drawing.Size(159, 20);
+            this.formOfDatePresentationToolStripMenuItem.Text = "Form of data presentation.";
+            // 
+            // listBoxesToolStripMenuItem
+            // 
+            this.listBoxesToolStripMenuItem.Name = "listBoxesToolStripMenuItem";
+            this.listBoxesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.listBoxesToolStripMenuItem.Text = "List Boxes";
+            this.listBoxesToolStripMenuItem.Click += new System.EventHandler(this.listBoxesToolStripMenuItem_Click);
+            // 
+            // dataGridViewToolStripMenuItem
+            // 
+            this.dataGridViewToolStripMenuItem.Name = "dataGridViewToolStripMenuItem";
+            this.dataGridViewToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.dataGridViewToolStripMenuItem.Text = "DataGridView";
+            this.dataGridViewToolStripMenuItem.Click += new System.EventHandler(this.dataGridViewToolStripMenuItem_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AutoGenerateColumns = false;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.timeDataGridViewTextBoxColumn,
+            this.valueDataGridViewTextBoxColumn,
+            this.commentDataGridViewTextBoxColumn});
+            this.dataGridView.DataSource = this.measurementsBindingSource;
+            this.dataGridView.Location = new System.Drawing.Point(38, 128);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(567, 381);
+            this.dataGridView.TabIndex = 13;
+            // 
+            // measurementsDataSet
+            // 
+            this.measurementsDataSet.DataSetName = "MeasurementsDataSet";
+            this.measurementsDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // measurementsBindingSource
+            // 
+            this.measurementsBindingSource.DataMember = "Measurements";
+            this.measurementsBindingSource.DataSource = this.measurementsDataSet;
+            // 
+            // measurementsTableAdapter
+            // 
+            this.measurementsTableAdapter.ClearBeforeFill = true;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // timeDataGridViewTextBoxColumn
+            // 
+            this.timeDataGridViewTextBoxColumn.DataPropertyName = "time";
+            this.timeDataGridViewTextBoxColumn.HeaderText = "time";
+            this.timeDataGridViewTextBoxColumn.Name = "timeDataGridViewTextBoxColumn";
+            // 
+            // valueDataGridViewTextBoxColumn
+            // 
+            this.valueDataGridViewTextBoxColumn.DataPropertyName = "value";
+            this.valueDataGridViewTextBoxColumn.HeaderText = "value";
+            this.valueDataGridViewTextBoxColumn.Name = "valueDataGridViewTextBoxColumn";
+            // 
+            // commentDataGridViewTextBoxColumn
+            // 
+            this.commentDataGridViewTextBoxColumn.DataPropertyName = "comment";
+            this.commentDataGridViewTextBoxColumn.HeaderText = "comment";
+            this.commentDataGridViewTextBoxColumn.Name = "commentDataGridViewTextBoxColumn";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1130, 596);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.commentListBox);
+            this.Controls.Add(this.valueListBox);
+            this.Controls.Add(this.timeListBox);
+            this.Controls.Add(this.idListBox);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.insertButton);
+            this.Controls.Add(this.commentTextBox);
+            this.Controls.Add(this.valueTextBox);
+            this.Controls.Add(this.commentLabel);
+            this.Controls.Add(this.valueLabel);
+            this.Name = "MainWindow";
+            this.Text = "Measurements: Insert Delete Update";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.measurementsDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.measurementsBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label valueLabel;
+        private System.Windows.Forms.Label commentLabel;
+        private System.Windows.Forms.TextBox valueTextBox;
+        private System.Windows.Forms.TextBox commentTextBox;
+        private System.Windows.Forms.Button insertButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.ListBox idListBox;
+        private System.Windows.Forms.ListBox timeListBox;
+        private System.Windows.Forms.ListBox valueListBox;
+        private System.Windows.Forms.ListBox commentListBox;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem formOfDatePresentationToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listBoxesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dataGridViewToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private MeasurementsDataSet measurementsDataSet;
+        private System.Windows.Forms.BindingSource measurementsBindingSource;
+        private MeasurementsDataSetTableAdapters.MeasurementsTableAdapter measurementsTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn timeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn valueDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn commentDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ WindowsFormSQL/MainWindow.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormSQL
+{
+    public partial class MainWindow : Form
+    {
+        private string connString = "Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename = C:\\Users\\Patryk\\source\\repos\\WindowsFormSQL\\WindowsFormSQL\\Measurements.mdf; Integrated Security = True";
+        //string connString = ""; DODAĆ NA LAPTOPIE INNE;
+        private Boolean isListBox = true;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            dataGridView.Visible = false;
+            loadlist();
+
+        }
+
+        private void insertButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection())
+                {
+                    conn.ConnectionString = connString;
+                    conn.Open();
+                    SqlCommand command = new SqlCommand();
+                    command.CommandText = "INSERT INTO Measurements (value, comment, time) VALUES ((@value), (@comment), (@time) )";
+
+                    DateTime dateTimeVar = DateTime.Now;
+                    command.Parameters.AddWithValue("@value", valueTextBox.Text);
+                    command.Parameters.AddWithValue("@comment", commentTextBox.Text);
+                    command.Parameters.AddWithValue("@time", dateTimeVar);
+
+                    command.Connection = conn;
+                    command.ExecuteNonQuery();
+                    conn.Close();
+                    loadlist();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+
+
+        }
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection())
+                {
+                    conn.ConnectionString = connString;
+                    conn.Open();
+                    SqlCommand command = new SqlCommand();
+                    command.CommandText = "DELETE FROM Measurements WHERE Id = @id";
+                    
+                    command.Parameters.AddWithValue("@id", idListBox.SelectedItem);
+
+                    command.Connection = conn;
+                    command.ExecuteNonQuery();
+                    conn.Close();
+                    loadlist();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection())
+                {
+                    conn.ConnectionString = connString;
+                    conn.Open();
+                    SqlCommand command = new SqlCommand();
+                    command.CommandText = "UPDATE Measurements SET value = @value, comment = @comment, time = @time WHERE Id = @id";
+                    DateTime dateTimeVar = DateTime.Now;
+                    command.Parameters.AddWithValue("@time", dateTimeVar);
+                    command.Parameters.AddWithValue("@id", idListBox.SelectedItem);
+                    command.Parameters.AddWithValue("@value", valueTextBox.Text);
+                    command.Parameters.AddWithValue("@comment", commentTextBox.Text);
+
+                    command.Connection = conn;
+                    command.ExecuteNonQuery();
+                    conn.Close();
+                    loadlist();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+
+        }
+    
+        private void loadlist()
+        {
+            if (isListBox)
+            {
+                idListBox.Items.Clear();
+                timeListBox.Items.Clear();
+                valueListBox.Items.Clear();
+                commentListBox.Items.Clear();
+
+                using (SqlConnection conn = new SqlConnection())
+                {
+                    conn.ConnectionString = connString;
+                    conn.Open();
+                    SqlCommand command = new SqlCommand("SELECT * FROM Measurements", conn);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            idListBox.Items.Add(reader[0].ToString());
+                            timeListBox.Items.Add(reader[1].ToString());
+                            valueListBox.Items.Add(reader[2].ToString());
+                            commentListBox.Items.Add(reader[3].ToString());
+
+                        }
+                    }
+                    conn.Close();
+                }
+            }
+            else
+            {
+                SqlConnection conn = new SqlConnection();
+                SqlCommand sqlCmd = new SqlCommand("SELECT * FROM Measurements", conn);
+                try
+                {
+                    if (conn.State == ConnectionState.Closed)
+                    {
+                        conn.ConnectionString = connString;
+                        conn.Open();
+                    }
+                    SqlDataAdapter sda = new SqlDataAdapter();
+                    sda.SelectCommand = sqlCmd;
+                    DataTable dbdataset = new DataTable();
+                    sda.Fill(dbdataset);
+                    BindingSource bSource = new BindingSource();
+
+                    bSource.DataSource = dbdataset;
+                    dataGridView.DataSource = dbdataset;
+                    sda.Update(dbdataset);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error 2");
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'measurementsDataSet.Measurements' table. You can move, or remove it, as needed.
+            this.measurementsTableAdapter.Fill(this.measurementsDataSet.Measurements);
+
+        }
+
+        private void listBoxesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            isListBox = true;
+            loadlist();
+            dataGridView.Visible = false;
+
+            valueTextBox.Enabled  = true;
+            valueLabel.Enabled  = true;
+            valueTextBox.Enabled  = true;
+
+            commentLabel.Enabled  = true;
+            commentListBox.Visible = true;
+            commentTextBox.Enabled  = true;
+
+            insertButton.Enabled  = true;
+            updateButton.Enabled  = true;
+            deleteButton.Enabled  = true;
+
+            timeListBox.Visible = true;
+            valueListBox.Visible = true;
+            idListBox.Visible = true;
+        }
+
+        private void dataGridViewToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            isListBox = false;
+            loadlist();
+            dataGridView.Visible = true;
+            valueTextBox.Enabled  = false;
+            valueLabel.Enabled  = false;
+            valueTextBox.Enabled  = false;
+
+            commentLabel.Enabled  = false;
+            commentListBox.Visible = false;
+            commentTextBox.Enabled  = false;
+
+            insertButton.Enabled  = false;
+            updateButton.Enabled  = false;
+            deleteButton.Enabled  = false;
+
+            timeListBox.Visible = false;
+            valueListBox.Visible = false;
+            idListBox.Visible = false;
+        }
+    }
+}
